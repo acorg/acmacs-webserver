a7feb63a0d9ff89af6740b46a732d6cebfa911d8
Eugene Skepner <github@skepner.eu>
2017-06-28 15:06:08 +0200

    uws-test

--- cc/uws-test.cc
+++ cc/uws-test.cc
@@ -1,8 +1,18 @@
+#include <string>
+#include <iostream>
 #include "uws.hh"
 
 int main()
 {
     uWS::Hub h;
+
+    h.onHttpRequest([](uWS::HttpResponse* respose, uWS::HttpRequest request, char *data, size_t length, size_t remainingBytes) {
+            std::cout << "URL: " << request.getUrl().toString() << " HOST:" << request.getHeader("host").toString() << std::endl;
+            if (data)
+                std::cout << "REQ data: " << data << std::endl;
+            std::string reply = "<html><body><h1>UWS-TEST</h1></body></html>";
+            respose->end(reply.c_str(), reply.size());
+        });
 
     h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
             ws->send(message, length, opCode);
