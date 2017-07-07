68f39ebaa82a2ef1bd0349d7f2eebf8d088477f2
Eugene Skepner <github@skepner.eu>
2017-07-07 14:08:18 +0200

    wspp-test

--- cc/wspp-http.hh
+++ cc/wspp-http.hh
@@ -4,11 +4,11 @@
 
 #pragma GCC diagnostic push
 #include "acmacs-base/boost-diagnostics.hh"
-#include "websocketpp/http/constants.hpp"
 #ifdef __clang__
 #pragma GCC diagnostic ignored "-Wconversion"
 #pragma GCC diagnostic ignored "-Wmissing-noreturn"
 #endif
+// for websocketpp::frame::opcode, websocketpp::config::asio::message_type
 #include "websocketpp/config/asio.hpp"
 #pragma GCC diagnostic pop
 
